370077036f3c06d825d1ac597a9445932ea86487
tharunroyal <130845068+tharunroyal@users.noreply.github.com>
2023-11-17 08:17:12 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`mongodb://127.0.0.1:27017/Hospital_DB`);
+mongoose.connect(`mongosh "mongodb+srv://hospital-api.w5qrrwc.mongodb.net/" --apiVersion 1 --username tharunkumart53`);
 
 const db = mongoose.connection;
 
